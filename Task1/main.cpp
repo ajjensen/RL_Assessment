aefeabc1abb36f30e3dd6ea52ebc2b10ccc1d56e
Alex Jensen <jensen.alexj@gmail.com>
2021-12-02 13:24:02 -0500

    - Minor edits to README.md - main.cpp accepts input  -Debugging not working on Mac, move to Linux for math-y part.

---
+++ Task1/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    string userPrompt = "Please enter an integer between 0-9999:";
+    int base;
+    cout << userPrompt << endl;
+    cin >> base;
+}
