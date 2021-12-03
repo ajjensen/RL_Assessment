78d02f9933464c701a60bb0479d87abbc9f9f18b
Alexander Jensen <jensen.alexj@gmail.com>
2021-12-03 09:50:14 -0500

    - Base parsed properly - Exponent assigned
    TO DO: perform exponential operation

--- Task1/main.cpp
+++ Task1/main.cpp
@@ -2,9 +2,35 @@
 #include <string>
 using namespace std;
 
-int main(){
-    string userPrompt = "Please enter an integer between 0-9999:";
-    int base;
+int main(void){
+    string userPrompt = "Please enter an integer between 0-99999:";
+    string base_str;
+
     cout << userPrompt << endl;
-    cin >> base;
+    cin >> base_str;
+    
+    /* Reverse base and assign to exponent */
+    string exp_str;
+    bool leading_zeros = true;
+
+    for( int i = base_str.size()-1; i >= 0; i = i-1 ){
+        if( leading_zeros & base_str[i] == '0') {
+        } else {
+            exp_str.push_back(base_str[i]);
+            leading_zeros = false;
+        }   
+    }
+
+    /* Cast base and exponent to integers */
+    size_t sz;
+    int base = stoi(base_str, &sz);
+    int exp = stoi(exp_str, &sz);
+
+    /* FIX ME: remove output */
+    cout << "base: " << base << endl;
+    cout << "exponent: " << exp << endl;
+
+    /* Perform exponential operation */
+
+    return 0;
 }
