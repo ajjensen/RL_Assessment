d91ddb0bd26b8e9c5f22c57118178deb984eec14
Alexander Jensen <jensen.alexj@gmail.com>
2021-12-03 12:24:13 -0500

    Functioning as required

--- Task1/main.cpp
+++ Task1/main.cpp
@@ -1,12 +1,67 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
+vector<int> mult(int x, vector<int> cur){
+    
+    // Initialize carry
+    int carry = 0;
+ 
+    // One by one multiply n with
+    // individual digits of res[]
+    for (int i = 0; i < cur.size(); i++) {
+        int prod = cur[i] * x + carry;
+        
+        // Store last digit of
+        // 'prod' in res[]
+        cur[i] = prod % 10;
+        
+        // Put rest in carry
+        carry = prod / 10;
+    }
+    
+    while (carry) {
+        cur.push_back(carry % 10);
+        carry = carry / 10;
+    }
+    
+    return cur;
+}
+
+void pow(int base, int exp){
+    
+    if(exp == 0 ){
+        std::cout << "1";
+        return;
+    }
+    
+    vector<int> res;
+    int temp = base;
+    
+    // Initialize res.ans
+    while (temp != 0) {
+        res.push_back(temp % 10);
+        temp = temp / 10;
+    }
+    
+    for(int i = 1; i < exp; i = i+1) {
+        res = mult(base, res);
+    }
+    
+    // Output Result
+    std::cout << "\nResult: " << base <<"^"<< exp << " is \n";
+    for (int i = res.size() - 1; i >= 0; i--){
+        std::cout << res[i];
+    }
+    std::cout << endl << endl;
+}
+
 int main(void){
-    string userPrompt = "Please enter an integer between 0-99999:";
+    string userPrompt = "Please enter an integer between 0-99999: ";
     string base_str;
 
-    cout << userPrompt << endl;
+    cout << userPrompt;
     cin >> base_str;
     
     /* Reverse base and assign to exponent */
@@ -26,11 +81,8 @@ int main(void){
     int base = stoi(base_str, &sz);
     int exp = stoi(exp_str, &sz);
 
-    /* FIX ME: remove output */
-    cout << "base: " << base << endl;
-    cout << "exponent: " << exp << endl;
-
     /* Perform exponential operation */
+    pow(base, exp);
 
     return 0;
 }
